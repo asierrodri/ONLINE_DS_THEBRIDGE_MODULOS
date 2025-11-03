d74fc7688aca9b523594f8f20fda310e4a51e5e0
unknown <asierrodriguezmurua@gmail.com>
2025-11-03 16:18:02 +0100

    Ejercicios resueltos dia 3

---
+++ 1-RampUp/Python/4-Funciones/Practica/funciones.py
@@ -0,0 +1,10 @@
+import math
+
+def cuadrado_prueba(lado):
+    return lado**2
+
+def triangulo_prueba(base, altura):
+    return 1/2 * base * altura
+
+def circulo_prueba(radio):
+    return math.pi * (radio**2)
